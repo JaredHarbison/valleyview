1260023f2928abbafab7660ffe54057440bc8bfd
Jared Harbison <jared.harbison@gmail.com>
2019-05-13 16:49:04 -0400

    added username, first_name, last_name columns to users table; added new attributes to params inside RegistrationsController

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2019_05_13_201149) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
