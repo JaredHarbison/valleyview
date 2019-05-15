67aa39be2d5b679b6a59c5ad039008c2ae0148f1
Jared Harbison <jared.harbison@gmail.com>
2019-05-15 11:56:17 -0400

    added a feature spec to test if a user can go to a single post from the home page

--- spec/features/user/logout_spec.rb
+++ spec/features/user/logout_spec.rb
@@ -2,18 +2,12 @@ require "rails_helper"
 
 RSpec.feature "Logout", :type => :feature do
   let(:user) { create(:user) }
-#####################################################
-######## can't solve this failing test below ########
-#####################################################
-#  scenario 'user successfully logs out', js: true do
-#    sign_in user
-#    visit root_path
-#    find('nav #user-settings').click
-#    find('nav a', text: 'Log out').click
-#    expect(page).to have_selector('nav a', text: 'Login')
-#  end
-#####################################################
-#####################################################
-#####################################################
+  scenario 'user successfully logs out', js: true do
+    sign_in user
+    visit root_path
+    find('nav #user-settings').click
+    find('nav a', text: 'Log out').click
+    expect(page).to have_selector('nav a', text: 'Login')
+  end
 
 end
