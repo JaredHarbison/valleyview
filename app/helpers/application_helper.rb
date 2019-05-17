ac94aa236ba5e39fb523fa1c893b59aa8be1d10f
Jared Harbison <jared.harbison@gmail.com>
2019-05-17 11:15:49 -0400

    setting 'username' vs 'name'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ require 'navigation_helper.rb'
 module ApplicationHelper
   include NavigationHelper
   include PostsHelper
+  include UsersHelper
 
   def base_title
     @base_title = "WerqBook"
--- db/migrate/20190513201149_devise_create_users.rb
+++ db/migrate/20190513201149_devise_create_users.rb
@@ -3,9 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username, null: false, default: ""
+      t.string :username#, null: false, default: ""
       t.string :first_name
-      t.string :last_name 
+      t.string :last_name
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
